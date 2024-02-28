cf2f1a058fadc1098fa4abde020e6187842a2a17
miryang1016 <156287271+miryang1016@users.noreply.github.com>
2024-02-28 22:16:06 +0900

    [Silver II] Title: 촌수계산, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,28 @@
+def family(s, g):
+    
+    if s == G:
+        return visited[s]
+    
+    else:
+        for i in rel[s]:
+            if visited[i] == -1:
+                visited[i] = visited[s] + 1
+                family(i, g)
+        
+    return visited[G]
+         
+N = int(input())
+S, G = map(int, input().split())
+M = int(input())
+
+visited = [-1] * (N + 1)
+rel = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    x, y = map(int, input().split())
+    rel[x].append(y)
+    rel[y].append(x)
+
+visited[S] = 0
+    
+print(family(S, G))

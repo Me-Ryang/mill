a21f75e378a972b1500c6ceac2c8062292eb0ea3
miryang1016 <156287271+miryang1016@users.noreply.github.com>
2024-03-30 00:19:43 +0900

    [Silver V] Title: 올림픽, Time: 92 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,25 +1,12 @@
-from collections import deque
 N, K = map(int, input().split())
 medal = [list(map(int, input().split())) for _ in range(N)]
 medal.sort(key = lambda x:(-x[1], -x[2], -x[3]))
-medal = deque(medal)
-result = [0] * N
 
-rank = 1
-result[0] = 1
-i, g, s, b = medal.popleft()
-while medal:
+for i in range(N):
+    if medal[i][0] == K:
+        idx = i
 
-    ni, ng, ns, nb = medal.popleft()
-    if ng == g and ns == s and nb == b:
-        result[ni - 1] = rank
-        rank += 1
-    else:
-        rank += 1
-        result[ni - 1] = rank
-    g, s, b = ng, ns, nb
-
-    if ni == K:
+for rank in range(1, N + 1):
+    if medal[idx][1:] == medal[rank - 1][1:]:
+        print(rank)
         break
-
-print(result[K - 1])

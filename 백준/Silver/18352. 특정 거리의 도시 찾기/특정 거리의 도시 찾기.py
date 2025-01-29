333ca02a48f022bd7ead016140fb3889c0bd68fc
Me-Ryang <156287271+Me-Ryang@users.noreply.github.com>
2025-01-29 15:42:56 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1376 ms, Memory: 100836 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -2,33 +2,34 @@ import sys
 from collections import deque
 
 N, M, K, X = map(int, sys.stdin.readline().split())
-city = [[] for _ in range(N + 1)]
-visited = [0] * (N + 1)
+
+lst = [[] for _ in range(N + 1)]
 for _ in range(M):
     A, B = map(int, sys.stdin.readline().split())
-    city[A].append(B)
+    lst[A].append(B)
+
+visited = [-1] * (N + 1)
+visited[X] = 0
+city = []
 
 q = deque([X])
-visited[X] = 1
-result = []
-
 while q:
     x = q.popleft()
 
-    if visited[x] == K + 1:
-        result.append(x)
+    if visited[x] == K:
+        city.append(x)
 
-    if visited[x] > K + 1:
+    if visited[x] > K:
         break
 
-    for i in city[x]:
-        if visited[i] == 0:
+    for i in lst[x]:
+        if visited[i] == -1:
             visited[i] = visited[x] + 1
             q.append(i)
 
-result.sort()
-if len(result) == 0:
-    print(-1)
+city.sort()
+if city:
+    for i in city:
+        print(i)
 else:
-    for r in range(len(result)):
-        print(result[r])
+    print(-1)

fcb44f30c1f0decdc7b6a681aebf7fd38ea77e60
Me-Ryang <156287271+Me-Ryang@users.noreply.github.com>
2025-02-02 00:38:50 +0900

    [Gold IV] Title: 인구 이동, Time: 5640 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,50 +1,57 @@
+import sys
 from collections import deque
 
 N, L, R = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * N for _ in range(N)]
+people = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
-def border(start_x, start_y):
-
-    q = deque()
-    q.append((start_x, start_y))
-    union = [(start_x, start_y)]
-    people = arr[start_x][start_y]
-    cnt = 1
+def part_visited(start_x, start_y, part):
+    q = deque([(start_x, start_y)])
+    visited[start_x][start_y] = part
+    p_list = [(start_x, start_y)]
+    p_people = people[start_x][start_y]
+    p_count = 1
 
     while q:
         x, y = q.popleft()
-        for d in range(4):
-            nx = x + dr[d]
-            ny = y + dc[d]
+        for k in range(4):
+            nx = x + dr[k]
+            ny = y + dc[k]
             if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == 0:
-                if abs(arr[x][y] - arr[nx][ny]) >= L and abs(arr[x][y] - arr[nx][ny]) <= R:
-                    visited[nx][ny] = 1
-                    people += arr[nx][ny]
-                    cnt += 1
+                if L <= abs(people[x][y] - people[nx][ny]) <= R:
+                    visited[nx][ny] = part
                     q.append((nx, ny))
-                    union.append((nx, ny))
+                    p_list.append((nx, ny))
+                    p_people += people[nx][ny]
+                    p_count += 1
 
-    for i, j in union:
-        arr[i][j] = people // cnt
+    people_move(p_list, p_people, p_count)
 
-    return cnt > 1
+def people_move(p_list, p_people, p_count):
+    if p_count > 1:
+        p_val = p_people // p_count
+        for x, y in p_list:
+            people[x][y] = p_val
+    else:
+        return False
 
-n = 0
+day = 0
 while True:
-    isMoved = False
+    part = 0
     visited = [[0] * N for _ in range(N)]
+    no_change = 0
     for i in range(N):
         for j in range(N):
-            if not visited[i][j]:
-                visited[i][j] = 1
-                if border(i, j):
-                    isMoved = True
-    if not isMoved:
-        break
-    n += 1
+            if visited[i][j] == 0:
+                part += 1
+                if not part_visited(i, j, part):
+                    no_change += 1
 
-print(n)
+    if no_change == N * N:
+        break
+    else:
+        day += 1
+
+print(day)

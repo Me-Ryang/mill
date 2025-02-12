84495d114b8f9119d252670df9dc88da15a8d071
Me-Ryang <156287271+Me-Ryang@users.noreply.github.com>
2025-02-12 15:59:17 +0900

    [Silver II] Title: 병사 배치하기, Time: 568 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+soldier = list(map(int, input().split()))
+soldier.reverse()
+
+dp = [1] * N
+for i in range(1, N):
+    for j in range(0, i):
+        if soldier[j] < soldier[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(N - max(dp))
